b4cedcc6f4df9f4d4a522ac6fab9a6d1aa41b1e1
aDubanchet <alexisdubanchet@yahoo.com>
2023-06-11 10:31:07 +0200

    new main

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
+# Neo4j
+Projet de NoSQL pour le cours de Remy Girodon à Télécom Saint-Etienne. 
+
 # Sources 
 Thanks to :
 https://github.com/elena/py2neo-quickstart
@@ -8,4 +11,10 @@ https://py2neo.org/2021.1/
 docker-compose.yml : détaille l'architecture du projet (une app python + une db neo4j)
 main.py : contient le script neo4j 
 
-
+# Quickstart
+## Démarrer hors du Docker
+````
+source env/bin/activate
+pip3 install -r requirements.txt
+python main.py
+```
--- main.py
+++ main.py
@@ -8,65 +8,93 @@ from py2neo import Graph, Node, Relationship
 # ----------------------------
 
 # Connexion à la base de données Neo4j
-graph = Graph("bolt://localhost:7687",auth=("neo4j","neo4j"))
+graph = Graph("neo4j://localhost:7687",auth=("neo4j","password"))
 
-# Fonction pour créer une entreprise
+
+# ----------------------------
+# Création des noeuds 
+# ----------------------------
 def creer_entreprise(nom):
     entreprise = Node("Entreprise", nom=nom)
-    graph.create(entreprise)
 
-# Fonction pour créer un utilisateur
+    tx = graph.begin()
+    tx.create(entreprise)
+    return entreprise
+
 def creer_utilisateur(nom, prenom, description, competences):
     utilisateur = Node("Utilisateur", nom=nom, prenom=prenom, description=description, competences=competences)
-    graph.create(utilisateur)
+    
+    tx = graph.begin()
+    tx.create(utilisateur)
+    return utilisateur
 
-# Fonction pour créer une relation "A travaillé pour"
+# ----------------------------
+# Relations entre les noeuds
+# ----------------------------
 def travailler_pour(utilisateur, entreprise, role, periode):
+    # creation d'une relation entre les noeuds
     relation = Relationship(utilisateur, "A_TRAVAILLE_POUR", entreprise, role=role, periode=periode)
-    graph.create(relation)
+    
+    tx = graph.begin()
+    tx.create(relation)
 
-# Fonction de recherche d'entreprises par nom
+# ----------------------------
+# Fonctions de recherche
+# ----------------------------
+
 def rechercher_entreprises_par_nom(nom):
     query = "MATCH (entreprise:Entreprise) WHERE entreprise.nom CONTAINS $nom RETURN entreprise"
     result = graph.run(query, nom=nom)
     return result
 
-# Fonction de recherche d'utilisateurs par nom
+
 def rechercher_utilisateurs_par_nom(nom):
     query = "MATCH (utilisateur:Utilisateur) WHERE utilisateur.nom CONTAINS $nom RETURN utilisateur"
     result = graph.run(query, nom=nom)
     return result
 
-# Fonction de suggestions de relations basée sur le travail en commun dans une entreprise donnée
+# ----------------------------
+# Suggestions
+# ----------------------------
+
 def suggestions_relation_travail(utilisateur, entreprise):
+    # suggestions de relations basée sur le travail en commun dans une entreprise donnée
     query = "MATCH (utilisateur)-[relation:A_TRAVAILLE_POUR]->(entreprise) WHERE entreprise.nom = $entreprise_nom AND NOT utilisateur = $utilisateur RETURN utilisateur"
     result = graph.run(query, entreprise_nom=entreprise, utilisateur=utilisateur)
     return result
 
-# Fonction de suggestions de relations basée sur les connaissances d'un utilisateur donné
 def suggestions_relation_connaissances(utilisateur):
+    # suggestions de relations basée sur les connaissances d'un utilisateur donné
     query = "MATCH (utilisateur)-[:A_TRAVAILLE_POUR]->(entreprise)<-[:A_TRAVAILLE_POUR]-(autre_utilisateur) WHERE NOT utilisateur = autre_utilisateur AND NOT (utilisateur)-[:CONNNAISSANCE]->(autre_utilisateur) RETURN autre_utilisateur"
     result = graph.run(query)
     return result
 
-# Exemples d'utilisation des fonctions
+
+
+# Test
 
 # Création d'une entreprise
-creer_entreprise("Relais de l'abbaye")
+entreprise = creer_entreprise("Relais de l'abbaye")
+
 
 # Création d'un utilisateur
-creer_utilisateur("Alexis", "Dbn", "Développeur Python", ["Python", "SQL", "Web"])
+utilisateur = creer_utilisateur("Alexis", "Dbn", "Développeur Python", ["Python", "SQL", "Web"])
+
 
 # Création d'une relation "A travaillé pour"
-utilisateur = graph.nodes.match("Utilisateur", nom="Doe").first()
-entreprise = graph.nodes.match("Entreprise", nom="Acme Corporation").first()
+print("utilisateur :",utilisateur)
+print("entreprise : ",entreprise)
 travailler_pour(utilisateur, entreprise, "Salarié", "2020-2022")
 
-# Recherche d'entreprises par nom
-resultat_entreprises = rechercher_entreprises_par_nom("Acme")
+# Recherche d'entreprise
+resultat_entreprises = rechercher_entreprises_par_nom("Relais de l'abbaye")
+print("-------------------")
+print("Recherche entreprise :")
 for entreprise in resultat_entreprises:
     print(entreprise["entreprise"]["nom"])
 
-# Recherche d'utilisateurs par nom
+# Recherche d'utilisateur
+print("-------------------")
+print("Recherche utilisateurs :")
 resultat_utilisateurs = rechercher_utilisateurs_par_nom("Dbn")
 print(resultat_utilisateurs)
